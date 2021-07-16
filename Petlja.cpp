ad714942ff5e4f626d42f43116cb2ee074a7795f
Andrej123456789 <83548580+Andrej123456789@users.noreply.github.com>
2021-07-16 12:31:38 +0200

    For command in C++

---
+++ Petlja.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{	
+	for (int prvi_broj = 0; prvi_broj <= 100; prvi_broj++)
+	{
+		cout << prvi_broj << " " << "Jabuka" <<endl;
+	}
+
+	system("pause");
+	return 0;
+}
