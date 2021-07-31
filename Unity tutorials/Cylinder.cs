93b6b2ffdfffd94c1b6b80260b961baf5421f96a
Andrej123456789 <83548580+Andrej123456789@users.noreply.github.com>
2021-07-31 12:54:34 +0200

    Update Cylinder.cs

--- Unity tutorials/Cylinder.cs
+++ Unity tutorials/Cylinder.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Cylinder : MonoBehaviour
 {
+    //Active and Destory
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class Cylinder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Destroy(gameObject);
     }
 }
